63d8490ce07c08e7db33b76cb1f254e37f675f3b
marshall235 <fredie1738@gmail.com>
2024-10-17 12:22:54 +0300

    Modifying the structure of the code

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -12,12 +12,14 @@ def safely_get_value(dct, key, default = None):
 
 
 import typing
+
+
 T = typing.TypeVar('T')
 
 
-def safely_get_value(dct: typing.Mapping, key: typing.Any, default: 
-        typing.Union[T, None] = None) -> \
-                typing.Union[typing.Any, T]:
+def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
+                     typing.Union[T, None] = None) -> \
+        typing.Union[typing.Any, T]:
     """
     More involved type annotations
     """
