cd37193d221ff6e918da66327435794689e87f8a
marshall235 <fredie1738@gmail.com>
2024-10-17 11:58:27 +0300

    Augment the following code with the correct duck-typed annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Augment the following code with
+the correct duck-typed annotations:
+
+# The types of the elements of the 
+input are not know
+def safe_first_element(lst):
+    if lst:
+        return lst[0]
+    else:
+        return None
+"""
+
+
+import typing
+
+
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> \
+        typing.Union[typing.Any, None]:
+    """
+    Duck Typed annotation
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
